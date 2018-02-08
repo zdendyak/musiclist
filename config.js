52b4df2d3411d7d4dd7553a8553f63e0f1bad2e9
olek <oleg_zdendyak@ukr.ner>
2018-02-08 15:41:39 +0200

    fix bug in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const configPath = './config.json';
-const parsed = JSON.parse(fs.readFileSync(configPath, 'UFT-8'));
+const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 
 // We have to export each object in order to access them separetely
 exports.crypto = parsed.crypto;
